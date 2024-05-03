c4da2d2c26a8c50f6fb40238ac5c5cb119dcdfe3
Ales Zima <ales@cpex.cz>
2024-05-03 09:06:07 +0200

    Added design and svelte

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      'primary-blue': 'hsl(237, 18%, 59%)',
+      'primary-red': 'hsl(345, 95%, 68%)',
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
