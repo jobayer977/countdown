3ec496920401afe933e7eda1e5049bac9160b536
Ales Zima <ales@cpex.cz>
2024-05-03 09:35:36 +0200

    Setup theme colors and sizes

---
+++ src/components/App.svelte
@@ -0,0 +1,3 @@
+<h1 class="uppercase tracking-widest text-md">We're launching soon</h1>
+<p class="text-primary-red text-xl">08</p>
+<p class="text-primary-blue uppercase tracking-[3px]">Days</p>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,21 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    colors: {
-      'primary-blue': 'hsl(237, 18%, 59%)',
-      'primary-red': 'hsl(345, 95%, 68%)',
+    fontFamily: { sans: ['Red Hat Text', 'system-ui', 'sans-serif'] },
+    fontSize: {
+      base: '0.875rem',
+      md: '1.375rem',
+      xl: '3.5rem',
     },
-
-    extend: {},
+    extend: {
+      colors: {
+        'primary-blue': 'hsl(237, 18%, 59%)',
+        'primary-red': 'hsl(345, 95%, 68%)',
+        'blue-600': 'hsl(236, 21%, 26%)',
+        'blue-800': 'hsl(235, 16%, 14%)',
+        'blue-900': 'hsl(234, 17%, 12%)',
+      },
+    },
   },
   plugins: [],
 };
